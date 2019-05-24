2d2aed2bad1c464244d0eba468f0a1b1c02d3c92
Chris Siena <zenatron@gmail.com>
2019-05-24 01:57:49 -0400

    Updated Composer & getPage test
    Updated the the composer file
    Moved the config into the class definition

--- tests/getPage.php
+++ tests/getPage.php
@@ -1,16 +1,13 @@
 <?php
 require('../vendor/autoload.php');
 
-//defines an array of the urls to archive, Requires http
-$config = [
+//defines the archiver
+$archiver = new web\Archiver([
   "file" => "urls.txt",
   "verbose" => false,
   "showFilePath" => false,
   "timezone" => "America/New_York"
-];
-
-//defines the archiver
-$archiver = new web\Archiver($config);
+]);
 
 //processes the urls with the params for console output
 echo $archiver->process_console();
